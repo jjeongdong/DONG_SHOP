7331b3c343da5aed00bf8864442470ad04085de7
jjeongdong <donghun3296@naver.com>
2023-07-05 23:33:35 +0900

    chore : 프로젝트 초기 세팅

---
+++ src/main/java/com/example/dong_shop/DongShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.dong_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DongShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dong_shop/DongShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dong_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
