babe01af168002b271fb576af0cdb35b717d549d
jjeongdong <donghun3296@naver.com>
2023-07-20 16:28:51 +0900

    fix : 화면에 사진 안보이는 버그 수정

--- src/main/java/com/example/dong_shop/config/WebMvcConfig.java
+++ src/main/java/com/example/dong_shop/config/WebMvcConfig.java
@@ -1,9 +1,11 @@
 package com.example.dong_shop.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Value(value = "${uploadPath}")
